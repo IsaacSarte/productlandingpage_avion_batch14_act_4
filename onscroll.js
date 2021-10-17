dd28fed5a4884e9ab8149a6c891926eb36a7423d
Isaac Sarte <aizakku09@gmail.com>
2021-10-17 14:59:48 +0800

    animation on scroll fadein

--- onscroll.js
+++ onscroll.js
@@ -24,33 +24,3 @@ faders.forEach(fader => {
     appearOnScroll.observe(fader);
 });
 
-// Fade from Left
-
-const fadersLeft = document.querySelectorAll('.fade-left');
-
-const appearOnScrollLeft = new IntersectionObserver(function(
-    entries,
-    appearOnScrollLeft
-) {
-    entries.forEach(entry => {
-        if(!entry.isIntersecting) {
-            return;
-        }
-        else {
-            entry.target.classList.add('appearLeft');
-            appearOnScrollLeft.unobserve(entry.target);
-        }
-    });
-}, appearOptions);
-
-fadersLeft.forEach(faderLeft => {
-    appearOnScrollLeft.observe(faderLeft);
-})
-
-// Slide in
-
-const sliders = document.querySelectorAll('.slide-in');
-
-sliders.forEach(slider => {
-    appearOnScroll.observe(slider);
-});
